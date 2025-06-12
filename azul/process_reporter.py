230d0891cc7b3831b948441b5cf95a0ad0dd6c6e
kris <christoffer.gang@yahoo.com>
2025-06-12 21:54:07 +0400

    Fix line length to comply with PEP8 for CI lint

--- azul/process_reporter.py
+++ azul/process_reporter.py
@@ -6,11 +6,14 @@ import argparse
 
 def get_processes():
     processes = []
-    for proc in psutil.process_iter(['pid', 'name', 'username', 'cpu_percent', 'memory_info']):
+    for proc in psutil.process_iter(
+        ['pid', 'name', 'username', 'cpu_percent', 'memory_info']
+    ):
         try:
             info = proc.info
             info['memory_usage'] = round(
-                info['memory_info'].rss / (1024 * 1024), 2)  # in MB
+                info['memory_info'].rss / (1024 * 1024), 2
+            )  # in MB
             del info['memory_info']
             processes.append(info)
         except (psutil.NoSuchProcess, psutil.AccessDenied):
@@ -33,14 +36,27 @@ def save_report(data, output_format='csv', filename='azul_process_report'):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description='Generate a report of running processes.')
-    parser.add_argument('-f', '--output-format',
-                        choices=['csv', 'json'], default='csv', help='Output format: csv or json')
-    parser.add_argument('-o', '--output', default='azul_process_report',
-                        help='Output filename (without extension)')
+        description='Generate a report of running processes.'
+    )
+    parser.add_argument(
+        '-f',
+        '--output-format',
+        choices=['csv', 'json'],
+        default='csv',
+        help='Output format: csv or json'
+    )
+    parser.add_argument(
+        '-o',
+        '--output',
+        default='azul_process_report',
+        help='Output filename (without extension)'
+    )
 
     args = parser.parse_args()
 
     processes = get_processes()
-    save_report(processes, output_format=args.output_format,
-                filename=args.output)
+    save_report(
+        processes,
+        output_format=args.output_format,
+        filename=args.output,
+    )
